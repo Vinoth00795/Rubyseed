f0d967f78bb3674482b30e0878ce9735d6cf0682
Vinoth Thomas <vinoth.thomas@icicletech.com>
2019-02-18 18:39:30 +0530

    Practice one with outputs

--- seed.rb
+++ seed.rb
@@ -8,3 +8,13 @@ hsh.each do |key, value|
     end
 end
 
+# Output
+
+# red is 32 under row 12
+# red is 32 under row 31
+# red is 32 under row 54
+# red is 32 under row 65
+# green is 12 under row 12
+# green is 12 under row 31
+# green is 12 under row 54
+# green is 12 under row 65
