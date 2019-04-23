7f48e43f40d6b80bf9c6305f1eea921bc5326e3c
Vinoth Thomas <vinoth.thomas@icicletech.com>
2019-04-23 23:10:10 +0530

    practice 6

---
+++ practice6.rb
@@ -0,0 +1,40 @@
+# Write a Ruby script which demonstrate use of blocks, lambd &proc.
+
+$array = [1,2,3,4,5]
+
+block = $array.map! {|n| "Blocks prints #{n}"} # single line block
+puts block
+
+my_proc = Proc.new {|a,b| puts "Proc Printed #{a} and #{b}"} # proc example
+
+my_lambda = lambda {|a,b| puts "Block Printed #{a} and #{b}"} # lambda example
+
+# Ex1
+puts my_proc.call(2,1)
+puts my_lambda.call(2,1)
+
+# Ex2
+puts my_proc.call(2)
+puts my_lambda.call(2)
+
+
+
+# Output
+# Blocks prints 1
+# Blocks prints 2
+# Blocks prints 3
+# Blocks prints 4
+# Blocks prints 5
+# Proc Printed 2 and 1
+
+# Block Printed 2 and 1
+
+# Proc Printed 2 and 
+
+# practice6.rb:10:in `block in <main>': wrong number of arguments (given 1, expected 2) (ArgumentError)
+#         from practice6.rb:18:in `<main>'
+
+# Comments
+# We can see that I have mentioned two arrguments in both, but supplied only one.
+# In proc it works but in lambda it won't. This is the difference between proc and lambda.
+# Lamnda has a strict arrgument checking, proc will exceute the result with the available data.
